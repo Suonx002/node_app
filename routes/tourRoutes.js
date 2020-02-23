cc01b67da2e51ac6ac1fb58c1938a9d947c82a2f
Vuthy <suonx002@gmail.com>
2020-02-23 16:47:17 -0600

    refactor to mvc model and add mongodb as database

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,15 +1,13 @@
 const express = require('express');
+
 const router = express.Router();
 
 const tourController = require('../controllers/tourController');
 
-// check valid param id
-router.param('id', tourController.checkID);
-
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 
 router
   .route('/:id')
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });

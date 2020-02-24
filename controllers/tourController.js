ccaf97c09dccaad667e0e924976f761271c1337c
Vuthy <suonx002@gmail.com>
2020-02-24 16:32:53 -0600

    adding pagination fields for tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -35,6 +35,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // Pagination fields
+    // page=2&limit=10
+    // skip(2).limit(10) => 1-10 page 1, 11-20 page 2, 21 -30 page 3
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // Execute query
     const tours = await query;
 

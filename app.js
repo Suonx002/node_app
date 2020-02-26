0c585b584f87898726558dfe060e8473c953a88e
Vuthy <suonx002@gmail.com>
2020-02-25 22:18:08 -0600

    refactor error handler middleware and create catchAsync for async function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -18,4 +21,11 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// catch errors (all verbs: get post put )
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// express handling errors middleware (error controller)
+app.use(globalErrorHandler);
 module.exports = app;

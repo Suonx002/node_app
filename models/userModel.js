347c5d462a5223cf67d418423e5f5220a058c569
Vuthy Suon <vuthy@Vuthys-MacBook-Pro.local>
2020-02-27 01:36:26 -0600

    create auth controller for signup with hashing password with bcrypt and create token with jsonwebtoken

---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!']
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please provide your email'],
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // this only work on CREATE and SAVE
+      validator: function(currentElement) {
+        return currentElement === this.password;
+      },
+      message: 'Password does not match'
+    }
+  },
+  photo: String
+});
+
+// mongoose middlewares
+userSchema.pre('save', async function(next) {
+  // only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // default is 10
+  // hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

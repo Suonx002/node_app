7b5e234e8a349cf953999d6137f28a61273a6d5d
Vuthy Suon <vuthy@Vuthys-MacBook-Pro.local>
2020-02-28 21:58:57 -0600

    adding geolocation model for user

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
 
+const User = require('./userModel');
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -75,11 +77,37 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      //lng first then lat
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
     createdAt: {
       type: Date,
       default: Date.now(),
       select: false
-    }
+    },
+    guides: Array
   },
   // options object
   {
@@ -97,6 +125,12 @@ tourSchema.virtual('durationWeeks').get(function() {
 tourSchema.pre('save', function(next) {
   // adding slugs
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 

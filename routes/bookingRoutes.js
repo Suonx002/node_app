259e23b6db1d123c94c0e18483e374c4e525dc92
Vuthy <suonx002@gmail.com>
2020-03-02 15:52:55 -0600

    add crud functionality for bookings

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -5,10 +5,25 @@ const router = express.Router();
 const bookingController = require('./../controllers/bookingController');
 const authController = require('./../controllers/authController');
 
+router.use(authController.protect);
+
 router.get(
   '/checkout-session/:tourId',
-  authController.protect,
+
   bookingController.getCheckoutSession
 );
+
+router.use(authController.authorize('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
